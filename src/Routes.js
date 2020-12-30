02f227f20199474be43bfde4e83a2aae29c84ce6
Vivek Javiya <61119120+codewithvk@users.noreply.github.com>
2020-12-30 21:02:28 +0530

    fn

---
+++ src/Routes.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import Home from './core/Home'
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/'  component={Home} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes
+// import React from "react";
+// import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import Home from "./core/Home";
+
+// const Routes = () => {
+//   return (
+//     <BrowserRouter>
+//       <Switch>
+//         <Route path="/" exact component={Home} />
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default Routes;
